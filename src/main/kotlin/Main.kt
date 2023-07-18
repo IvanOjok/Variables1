d70945d318413154de8896b51ba874216abdb21e
OiJohnSteven <oisteven018@gmail.com>
2023-07-18 19:29:57 +0300

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+/*
+ *
+ * print executes a single line statement and continues to the next line of code
+ * e for execution but doesn't break at the end of the line
+ *
+ *
+ *  */
+
+fun main() {
+    //Println prints a statement and breaks after the line is executed
+
+
+    //println("Hello World");
+    //println("Hello World 1");
+    //print("Hello World 2");
+    //print("Hello World 3");
+
+    //declaring variables
+    // val - value , var - varying
+
+    val x = 1
+    var y = 5
+    y = 70
+    val z = x + y
+    //print("The sum of $x and $y is $z")
+
+    //Data type defines the type and kind of data being stored
+    //Integers (Int, Long) Decimal points (Floats (Float), Doubles (Double)),
+    // Text (Characters, Strings), Logic (Boolean), Bytes
+
+
+    val num1: Int = 12   //- 4 bytes
+    val num2: Long = 12   // - 8 bytes
+    val num3 = num1 + num2
+
+
+    val flo: Float = 0.3f   //- 8 bytes
+    val dor: Double = 0.3    //- 16 bytes
+    print(flo)
+
+    val c: Char = 'A'      // - 1 byte
+    val s: String = "This is the kotlin class"     //- 2 bytes
+
+    val t:Boolean = true
+
+
+}
