e530c28f65508c3878e7abc84912fd95470f5f60
OiJohnSteven <oisteven018@gmail.com>
2023-07-20 19:31:12 +0300

    Nullability and conditionals

--- src/main/kotlin/Conditionals.kt
+++ src/main/kotlin/Conditionals.kt
@@ -46,4 +46,34 @@ fun main() {
     }
 
 
+    /**
+    1. Solve the equation x2+10x+25 = 0  - x = (-b+-Sqrt(b2-4ac))/2a
+     2. A car is moving at a speed of 1000m/s at an acceleration of 25m/s/s
+     Find the time it will take to move a distance of 5km
+    Hint - acceleration = speed/time
+           speed = distance/time
+
+     3. A person has four option of choosing between;
+    pizza, chicken, pancakes or cake. If they choose pizza, they'll go to pizza hut,
+     if chicken, they'll go to kfc, if pancakes, they'll go to foodhub,
+     if cake, they'll go to cakely. Write
+     -a program using an if statement that captures the persons choice and prints the
+     selected restaurant
+     -same program using a when statement
+
+     4. Solve the equation x3+7x2+10x = 0
+
+     5. A person is moving to his village with a car moving at 20km/h and He has many
+    alternative routes.
+    If he uses route1, he'll move a distance of 5000km
+    If he uses route2, he'll move a distance of 25000km
+    If he uses route3, he'll move a distance of 50000km
+    If he uses another, he'll move a distance of 10000km
+
+     Find the time it will take him to reach the village using the separate routes
+     -using an if statement
+     -using a when statement
+
+    */
+
 }
