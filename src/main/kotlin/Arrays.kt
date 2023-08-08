1391b43c3289426762e45195f609ebece0e6b018
OiJohnSteven <oisteven018@gmail.com>
2023-08-08 19:01:13 +0300

    Collections; lists, sets and maps

--- src/main/kotlin/Arrays.kt
+++ src/main/kotlin/Arrays.kt
@@ -1,48 +1,71 @@
 fun main() {
-    //Non-primitive array
-    val firstArray = arrayOf("Kenya", "Uganda", "Tanzania", 8, true)
-
-    //primitive array
-    val secondArray = Array<Int>(5, {6})
-
-
-//    firstArray[0] = 9
-//    firstArray[1] = 10
-//    firstArray[2] = 9
-//    firstArray[3] = 10
-//    firstArray[4] = 9
-//    firstArray[5] = 9
-
-//    for (x in secondArray) {
-//        println(x)
+//    //Non-primitive array
+//    val firstArray = arrayOf("Kenya", "Uganda", "Tanzania", 8, true)
+//
+//    //primitive array
+//    val secondArray = Array<Int>(5, {6})
+//
+//
+////    firstArray[0] = 9
+////    firstArray[1] = 10
+////    firstArray[2] = 9
+////    firstArray[3] = 10
+////    firstArray[4] = 9
+////    firstArray[5] = 9
+//
+////    for (x in secondArray) {
+////        println(x)
+////    }
+////
+////    println(secondArray.size)
+//
+//    val x = Array<String>(20, {""})
+//    val s = IntArray(20, {7})
+//    val d = DoubleArray(4)
+//    val k = CharArray(5)
+//    val c = BooleanArray(6)
+//
+//    k[0] = 't'
+//    for (i in k) {
+//       // println(i)
 //    }
 //
-//    println(secondArray.size)
+//    x.forEach {
+//      //  print(it)
+//    }
+//
+//
+//    for (i in  100 downTo 0) {
+//        println(i)
+//    }
+//
+//
+//    //1. create an array of 50 integers and use a for loop to print out the names of the integers
+//    //2. create an array of 6 doubles and print out the sum of all elements in the array
+//    //3. in a class, there are five students; Ivan, Aron, Grace, Nisah, and Christine. Create an array
+//    //  in this order and afterwards, print the result of the elements in alphabetical order
 
-    val x = Array<String>(20, {""})
-    val s = IntArray(20, {7})
-    val d = DoubleArray(4)
-    val k = CharArray(5)
-    val c = BooleanArray(6)
-
-    k[0] = 't'
-    for (i in k) {
-       // println(i)
+    //val myArray = arrayOf(1, 6,9,23,78,90, 89)
+    val disc = Array<Int>(100) { it }
+    //20, 50, 87
+    val d = arrayOf("", "nine")
+    val lastDigit = arrayOf("", "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+            "seventeen","eighteen", "nineteen")
+    val firstDigit = arrayOf("", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+            "eighty", "ninety")
+    for (i in disc.indices) {
+        if (disc[i]<=19) {
+            println(lastDigit[i])
+        } else {
+            val f = disc[i]/10
+            val l = disc[i]%10  
+            println(firstDigit[f] + lastDigit[l])
+        }
     }
 
-    x.forEach {
-      //  print(it)
-    }
+//    val x = arrayOf("d", "g", "t")
+//    x.forEach { it }
 
-
-    for (i in  100 downTo 0) {
-        println(i)
-    }
-
-
-    //1. create an array of 50 integers and use a for loop to print out the names of the integer
-    //2. create an array of 6 doubles and print out the sum of all elements in the array
-    //3. in a class, there are five students; Ivan, Aron, Grace, Nisah, and Christine. Create an array
-    //  in this order and afterwards, print the result of the elements in alphabetical order
 
 }
---
+++ src/main/kotlin/Collections.kt
@@ -0,0 +1,71 @@
+//mutable or immutable multiple number of elements
+//lists, sets, maps
+
+fun main() {
+
+//    //immutable list
+//    val myList = listOf(1, 4, 6, 7, 9, "rest", "tress")
+//    val list = List<Int>(45,  {9})
+//
+//    //mutable list
+//    val mList = mutableListOf("4", "5", "7", "6")
+//    mList.add(4, "tree")
+//    mList.add(5, "tree5")
+//    mList.add( "tree6")
+//    mList.add("")
+//
+//    val mList2 = MutableList<Int>(4){ 9 }
+////    mList2.add(7)
+////    mList2.add(90)
+////    mList2.forEach{
+////        println(it)
+////    }
+//
+//    for (i in 9 downTo 2) {
+//        mList2.add(i)
+//    }
+//    for (x in mList2) {
+//        println(x)
+//    }
+
+    /// Sets
+//    val set1 = setOf("3", 8, "7", "tree", "3", 9, "7", "6", "tree",)
+//    val set2:Set<Int> = setOf(4, 7, 2, 8)
+//
+//    set1.forEach {
+//       // println(it)
+//    }
+//
+//    val mSet1 = mutableSetOf("ree", "tree", 4, 6, 4, "tree", "ree")
+//    val mSet2:MutableSet<Int> = mutableSetOf(5)
+//    mSet1.add("tree")
+//    mSet1.add(5)
+//    mSet1.forEach {
+//        println(mSet1)
+//        println(it)
+//    }
+
+    //map
+    val map1 = mapOf(1 to "one", 2 to "two", "3" to "three" )
+    for (i in map1){
+        //println(i.value)
+    }
+
+    val myMap = mutableMapOf("1" to 1, "two" to 2, 3 to "three")
+    myMap.put("1", "3")
+    myMap.replace(3, "thirty")
+    //mMap1.values.add("r")
+    for (i  in myMap) {
+        println(i)
+    }
+
+
+    //1. Create a list of 10 integers; 89, 45, 23, 5, 67, 12, 55, 89, 54, 55
+   // and print them out in ascending order
+    //2. assuming a group A contains elements {a, b, c, d, e , a, g, r, f}
+    //  group B contains {ant, bee, elephant, dog, cat}. Create a map that links the two
+//  groups using their first characters
+    //3. after implementing 2 above, add five entries that map five unique characters to
+// the respective animal name
+
+}
