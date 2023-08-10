e9f07e7535dc1894c62587c78bc0ce267bffcefe
OiJohnSteven <oisteven018@gmail.com>
2023-08-10 17:36:56 +0300

    Assignment

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -46,18 +46,18 @@ fun main() {
 //    }
 
     //map
-    val map1 = mapOf(1 to "one", 2 to "two", "3" to "three" )
-    for (i in map1){
-        //println(i.value)
-    }
-
-    val myMap = mutableMapOf("1" to 1, "two" to 2, 3 to "three")
-    myMap.put("1", "3")
-    myMap.replace(3, "thirty")
-    //mMap1.values.add("r")
-    for (i  in myMap) {
-        println(i)
-    }
+//    val map1 = mapOf(1 to "one", 2 to "two", "3" to "three" )
+//    for (i in map1){
+//        //println(i.value)
+//    }
+//
+//    val myMap = mutableMapOf("1" to 1, "two" to 2, 3 to "three")
+//    myMap.put("1", "3")
+//    myMap.replace(3, "thirty")
+//    //mMap1.values.add("r")
+//    for (i  in myMap) {
+//        println(i)
+//    }
 
 
     //1. Create a list of 10 integers; 89, 45, 23, 5, 67, 12, 55, 89, 54, 55
@@ -67,5 +67,33 @@ fun main() {
 //  groups using their first characters
     //3. after implementing 2 above, add five entries that map five unique characters to
 // the respective animal name
+
+    val groupA = listOf("a", "b", "c", "d", "e" , "a", "g", "r", "f")
+    val groupB = listOf("ant", "bee", "elephant", "dog", "cat", "apple")
+    val map = mutableMapOf<String, String>()
+    for(i in groupA){
+        println("i is $i")
+        for(j in groupB) {
+            println("j is $j")
+            if (i.first() == j.first()) {
+                println("similar found; $i == $j")
+                map.put(i, j)
+            }
+//            else {
+//                map.put(i, "_")
+//            }
+        }
+    }
+
+    map.forEach {
+        println(it)
+    }
+    map.put("r", "rat")
+
+
+    val c = mapOf(1 to 1, 1 to 2, 3 to 3, 4 to 4)
+    c.forEach {
+        println(it)
+    }
 
 }
